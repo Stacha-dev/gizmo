cb3a324b36ac0b748485a2654c5ad76ab6a03790
oliverstasa <oliver.stasa@gmail.com>
2021-01-30 03:22:37 +0000

    remove comments

--- index.php
+++ index.php
@@ -2,16 +2,3 @@
 
   include './php/ini.php';
   include './php/layout.php';
-
-  //hh
-  /*
-  if (isset($_GET['g']) ||
-      $_SERVER['REMOTE_ADDR'] == '89.102.173.82' ||
-      $_SERVER['REMOTE_ADDR'] == '89.103.146.187' ||
-      $_SERVER['REMOTE_ADDR'] == '89.176.25.153' ||
-      $_SERVER['REMOTE_ADDR'] == '195.113.72.125' ||
-      $_SERVER['REMOTE_ADDR'] == '193.9.112.244') {
-  } else {
-    include './php/default.php';
-  }
-  */
